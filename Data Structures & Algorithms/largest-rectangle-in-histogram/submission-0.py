d639c1a516df8de7cdb8391fc3700178fc245e52
sahilsssingh <118068838+sahilsssingh@users.noreply.github.com>
2026-07-18 11:17:38 +0530

    Add: largest-rectangle-in-histogram - submission-0

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        max_area = 0
+        nse = self.NSE(heights)
+        pse = self.PSE(heights)
+
+        for i in range(0 , len(heights)):
+            max_area = max(max_area , heights[i] * (nse[i] - pse[i] - 1))
+        return max_area
+
+    def NSE(self, heights):
+        stack = []  # will store INDICES
+        ans = [len(heights)] * len(heights)  # default: no smaller element found → "end of array"
+
+        for i in range(len(heights) - 1, -1, -1):
+            while len(stack) > 0 and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if len(stack) > 0:
+                ans[i] = stack[-1]
+            stack.append(i)
+        return ans
+
+    def PSE(self, heights):
+        stack = []  # will store INDICES
+        ans = [-1] * len(heights)  # default: no smaller element found → "before start of array"
+
+        for i in range(0, len(heights)):
+            while len(stack) > 0 and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if len(stack) > 0:
+                ans[i] = stack[-1]
+            stack.append(i)
+        return ans
